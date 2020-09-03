57625335e86bd51d5a3012346d62f26cc576baec
hartmanb1 <70448734+hartmanb1@users.noreply.github.com>
2020-09-02 21:27:28 -0400

    Create Program.cs

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Display Hell World!
+            // Require press of any key to continue
+            Console.WriteLine("Press any key to continue.");
+            Console.WriteLine("Hello World!");
+            Console.ReadKey(true);
+        }
+    }
+}
